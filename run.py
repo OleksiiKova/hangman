65458673ee63fe32537fc798c54d00d5a63bebe5
Oleksii Kovalenko <aleshka0729@gmail.com>
2024-03-17 15:41:37 +0000

    Print hangman depending on the number of wrong answers.

--- run.py
+++ run.py
@@ -10,3 +10,78 @@ random_word = random.choice(list_of_words)
 
 print(random_word)
 
+# Encrypted print of a random word using underscore
+for x in random_word:
+    print("_", end=" ")
+
+print("")
+
+def print_hangman(wrong):
+    """
+    Print hangman depending on the number of wrong answers.
+    """
+    if(wrong == 0):
+        print("      ┍——————┑   ")
+        print("      |      |   ")
+        print("      |          ")
+        print("      |          ")
+        print("      |          ")
+        print("      |          ")
+        print("      |          ")
+        print("     /|\         ")
+    elif(wrong == 1):
+        print("      ┍——————┑   ")
+        print("      |      |   ")
+        print("      |      O   ")
+        print("      |          ")
+        print("      |          ")
+        print("      |          ")
+        print("      |          ")
+        print("     /|\         ")
+    elif(wrong == 2):
+        print("      ┍——————┑   ")
+        print("      |      |   ")
+        print("      |      O   ")
+        print("      |      |   ")
+        print("      |      |   ")
+        print("      |          ")
+        print("      |          ")
+        print("     /|\         ")
+    elif(wrong == 3):
+        print("      ┍——————┑   ")
+        print("      |      |   ")
+        print("      |      O   ")
+        print("      |     /|   ")
+        print("      |      |   ")
+        print("      |          ")
+        print("      |          ")
+        print("     /|\         ")
+    elif(wrong == 4):
+        print("      ┍——————┑   ")
+        print("      |      |   ")
+        print("      |      O   ")
+        print("      |     /|\\ ")
+        print("      |      |   ")
+        print("      |          ")
+        print("      |          ")
+        print("     /|\         ")
+    elif(wrong == 5):
+        print("      ┍——————┑   ")
+        print("      |      |   ")
+        print("      |      O   ")
+        print("      |     /|\\ ")
+        print("      |      |   ")
+        print("      |     /    ")
+        print("      |          ")
+        print("     /|\         ")
+    elif(wrong == 6):
+        print("      ┍——————┑   ")
+        print("      |      |   ")
+        print("      |      O   ")
+        print("      |     /|\\ ")
+        print("      |      |   ")
+        print("      |     / \\ ")
+        print("      |          ")
+        print("     /|\         ")           
+        
+print_hangman(0)
