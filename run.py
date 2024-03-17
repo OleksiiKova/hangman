268cffb48637f963b4ba97e6e178c7f4f866a904
Oleksii Kovalenko <aleshka0729@gmail.com>
2024-03-17 15:12:53 +0000

    Choose random word from list

--- run.py
+++ run.py
@@ -1,3 +1,12 @@
-words_list = ["Apple", "Banana", "Orange", "Pineapple", "Strawberry", "Watermelon", "Mango", "Grape", "Cherry", "Kiwi", 
+import random
+
+# List of words for Hangman game
+list_of_words = ["Apple", "Banana", "Orange", "Pineapple", "Strawberry", "Watermelon", "Mango", "Grape", "Cherry", "Kiwi", 
               "Lemon", "Peach", "Pear", "Raspberry", "Blueberry", "Carrot", "Potato", "Tomato", "Cucumber", "Broccoli", 
               "Spinach", "Lettuce", "Pepper", "Onion", "Garlic", "Cauliflower", "Zucchini", "Eggplant", "Pumpkin", "Radish"]
+
+# Choose random word from list
+random_word = random.choice(list_of_words)
+
+print(random_word)
+
