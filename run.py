796a24be43a41d9b9b70fa411b050011c0a3233c
Oleksii Kovalenko <aleshka0729@gmail.com>
2024-03-23 18:18:17 +0000

    Wire up APIs and confirm access to google sheet

--- run.py
+++ run.py
@@ -1,4 +1,21 @@
 import random
+import gspread
+from google.oauth2.service_account import Credentials
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file("creds.json")
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('hangman')
+
+scores = SHEET.worksheet('scores')
+data = scores.get_all_values()
+print(data)
 
 # List of words for Hangman game
 list_of_words = ["Apple", "Banana", "Orange", "Pineapple", "Strawberry", "Watermelon", "Mango", "Grape", "Cherry", "Kiwi", 
@@ -190,4 +207,4 @@ def get_username():
     username = input("Please, enter your name: ")
     return username
 
-main()
+# main()
